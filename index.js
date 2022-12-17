abbc90d227d8ffc7fb6a7bfd0b8e1dc82e6a7e1c
bhaveshcodes <120417108+bhaveshcodes@users.noreply.github.com>
2022-12-18 00:11:26 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,40 +6,39 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 // MongoDB
-const { connectToDb, getDb } = require('./db')
-const { ObjectId } = require('mongodb')
+// const { connectToDb, getDb } = require('./db')
+// const { ObjectId } = require('mongodb')
 
 // db connection
-let db
-const PORT=process.env.PORT
-connectToDb((err) => {
-    if (!err) {
-        app.listen(PORT || 3010, () => {
-         })
-        db = getDb()
-    }
-})
+// let db
+// const PORT=process.env.PORT
+// connectToDb((err) => {
+//     if (!err) {
+//         app.listen(PORT || 3010, () => {
+//          })
+//         db = getDb()
+//     }
+// })
 
 app.use(express.json())
 
-// app.listen(3010,console.log("Api Project-2 running on port 3010"))
-
+ 
 app.get('/', (req, res) => {
    
-    let blogs = []
-     db.collection('blogs')
-        .find()
-        .sort({ _id: -1 })
-        .forEach(blog => blogs.push(blog))
-        .then(() => {
+    // let blogs = []
+    //  db.collection('blogs')
+    //     .find()
+    //     .sort({ _id: -1 })
+    //     .forEach(blog => blogs.push(blog))
+    //     .then(() => {
              
-            res.json(blogs)
-            // res.end(JSON.stringify(blogs))
-            // res.writeHead(200, blogs) 
+    //         res.json(blogs)
+             
           
-        })
-        .catch((err) => {
-             res.status(500).json({ error: 'could not get data' })
-        })
+    //     })
+    //     .catch((err) => {
+    //          res.status(500).json({ error: 'could not get data' })
+    //     })
 
+    res.json("hi!")
 })
